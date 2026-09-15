d34a6a431810d056ea46231aada1d3f6e1a57c92
AnthonyFR04 <afinerobo@gmail.com>
2026-09-15 23:53:44 +0100

    Add working binary search solution

--- 704. Binary Search/solution.py
+++ 704. Binary Search/solution.py
@@ -1,17 +1,14 @@
+from math import floor
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        cur_pointer = len(nums) // 2
         start_pointer = 0
-        end_pointer = len(nums)
-        while True:
+        end_pointer = len(nums) -1
+        while start_pointer <= end_pointer:
+            cur_pointer = start_pointer + floor((end_pointer-start_pointer)/2)
             if nums[cur_pointer] < target:
-                start_pointer = cur_pointer
-                cur_pointer = (end_pointer + cur_pointer) // 2
+                start_pointer = cur_pointer + 1
             elif nums[cur_pointer] > target:
-                end_pointer = cur_pointer
-                cur_pointer //= 2
+                end_pointer = cur_pointer -1
             else:
                 return cur_pointer
-            if (cur_pointer == end_pointer) or (cur_pointer == start_pointer):
-                break
         return -1
